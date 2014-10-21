28d527ce6241e49ee3e932bd118d04676be6b255
Gwen Au <quyen_au@hotmail.com>
2014-10-21 16:07:09 +0100

    only 1 addition seems to be working so far.

--- js/main.js
+++ js/main.js
@@ -5,26 +5,31 @@
   app.controller('ShoppingController' , ["$scope" ,function($scope) {
 
     $scope.items = items;
+    $scope.newItem = false;
 
-    
+    $scope.addItem = function () {
+      $scope.items.push($scope.newItem)
+      $scope.newItem = false;
+      $scope.itemForm.$setPristine()      
+    }
 
   }])
 
   items = [  
     {  
       name: "Shampoo",  
-      price: 1,  
+      quantity: 1,  
       done:  false 
     },  
     {  
       name: "Conditioner",  
-      price: 2,  
+      quantity: 2,  
       done: false  
     },  
     {
       name: "Toilet paper",  
-      price: 6,  
-      description: false 
+      quantity: 6,  
+      done: false 
     }  
   ]
 
