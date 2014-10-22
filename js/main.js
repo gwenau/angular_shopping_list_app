3c05e8ef888b9ae9db4379520196a86668d77789
Gwen Au <quyen_au@hotmail.com>
2014-10-22 16:06:56 +0100

    Comments removed and syntax made consistent as possible.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Commented version
+
 (function() {
 
   var app = angular.module('shoppingListApp', ['ngAnimate']);
@@ -11,7 +11,7 @@
     $scope.addItem = function () {
 
     var id = $scope.items.length + 1;
-
+      
       $scope.items.push({
         id: id,
         name: $scope.item.name,
@@ -23,39 +23,26 @@
     }
 
     $scope.removeItem = function (item) {
-      // $scope.items.delete(item)
-      console.log(item);
-      console.log($scope)
-      remove($scope.items, item)
+      $scope.remove($scope.items, item)
     }
 
-    function remove(arr, item) {
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
-      // var i = 0, is the start, l = arr.length, determining the length of an array without hard coding it in.
-      // i < l, the conditions to run the loop
-      // i++ an increment method to signal what to do next in the loop. 
+    $scope.remove = function (arr, item) {
       for (var i = 0, l = arr.length; i < l; i++) {
           if (arr[i] === item) {
               arr.splice(i, 1);
-              // As soon as the function has been complete, break stops the irration but if you want to delete multiple items, take this out. 
-              break;
           }
       }
     }
 
     $scope.editItem = function (item){
-      console.log(item);
       $scope.item = item;
       $scope.saveButton = true;
-      // $scope.edit = true;
     }
 
     $scope.update = function (item){
-      console.log("update", item)
       $scope.saveButton = false;
       $scope.item = false;
       $scope.itemForm.$setPristine(); 
-      // $scope.edit = false;
     }
 
   }])
