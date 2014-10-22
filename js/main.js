9cd13059e71360a664d7c4c6d5fde2053733e9b1
Gwen Au <quyen_au@hotmail.com>
2014-10-22 11:30:19 +0100

    The delete function now works.

--- js/main.js
+++ js/main.js
@@ -4,44 +4,44 @@
 
   app.controller('ShoppingController' , ["$scope" ,function($scope) {
 
-    $scope.items = items;
+    $scope.items = [];
     $scope.newItem = false;
 
     $scope.addItem = function () {
+
+    var id = $scope.items.length + 1;
+
       $scope.items.push({
+        id: id,
         name: $scope.newItem.name,
         quantity: $scope.newItem.quantity,
         done: false
       });
-
-
-        // this.todos.push({
-        //     name: this.todoName,
-        //     estimate: this.todoEstimate,
-        //     done: false
-        // });
       $scope.newItem = false;
       $scope.itemForm.$setPristine()      
     }
 
-  }])
+    $scope.removeItem = function (item) {
+      // $scope.items.delete(item)
+      console.log(item);
+      console.log($scope)
+      remove($scope.items, item)
+    }
 
-  items = [  
-    {  
-      name: "Shampoo",  
-      quantity: 1,  
-      done:  false 
-    },  
-    {  
-      name: "Conditioner",  
-      quantity: 2,  
-      done: false  
-    },  
-    {
-      name: "Toilet paper",  
-      quantity: 6,  
-      done: false 
-    }  
-  ]
+    function remove(arr, item) {
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+      // var i = 0, is the start, l = arr.length, determining the length of an array without hard coding it in.
+      // i < l, the conditions to run the loop
+      // i++ an increment method to signal what to do next in the loop. 
+      for (var i = 0, l = arr.length; i < l; i++) {
+          if (arr[i] === item) {
+              arr.splice(i, 1);
+              // As soon as the function has been complete, break stops the irration but if you want to delete multiple items, take this out. 
+              break;
+          }
+      }
+    }
+
+  }])
 
 })();
